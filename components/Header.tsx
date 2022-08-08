4b1ca19014348abdeaa0d0e02a29cca4fb06f252
Brizo699 <97060331+Brizo699@users.noreply.github.com>
2022-08-09 05:33:41 +0800

    Header Updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,39 +5,44 @@ import { SearchIcon } from "@heroicons/react/outline";
 import iconiclogo from "../public/iconiclogo.png";
 function Header() {
   return (
-    <header className=" space-x-4 py-2 font-author tracking-wide">
-      <div
-        className="relative flex justify-center 
-        w-full h-14 my-2"
-      >
-        <Image
-          className=""
-          src={iconiclogo}
-          layout="fill"
-          objectFit="contain"
-        />
-      </div>
-      <div className="flex">
-        <Link href="/">
-          <p className="text-2xl text-white px-4 cursor-pointer">Pricing</p>
-        </Link>
+    <header className="font-author space-x-2 px-5 py-2 bg-black bg-opacity-20">
+      <div className="flex lg:flex-row lg:justify-between w-full h-full items-center flex-col justify-center ">
+        {/* Logo */}
+        <div className="relative lg:h-16 lg:w-48 h-20 w-60">
+          <Image
+            className=""
+            src={iconiclogo}
+            layout="fill"
+            objectFit="contain"
+            height="100px"
+            width="100px"
+          />
+        </div>
         {/* Search */}
-        <div className="flex bg-gray-100 rounded overflow-hidden flex-1 px-5 ">
-          <div className="flex items-center justify-center pl-3">
+        <div className="flex bg-black bg-opacity-50 rounded-md flex-1 w-full mx-10">
+          <div className="flex items-center pl-3">
             <SearchIcon className="flex-shrink-0 h-6 w-6 text-gray-400" />
           </div>
           <input
             type="text"
             placeholder="Search packages"
-            className="bg-transparent outline-none p-3 flex-1 text-xl placeholder-gray-400"
+            className="bg-transparent outline-none p-3 text-xl placeholder-gray-400 tracking-wide text-white "
           />
-          <button className="bg-gray-100 text-black text-xl px-4">
-            Search
-          </button>
         </div>
-        <div className="text-white px-4">
-          <button className="p-2 ">Sign in</button>
-          <button className="p-2">Sign up</button>
+        {/* Pricing & Sign in */}
+        <div className="text-white flex text-lg sm:text-xl items-center sm:space-x-6 rounded-lg mt-5 mb-5 lg:mt-0 lg:mb-0 ">
+          <Link href="/">
+            <p className="headerLink">Pricing</p>
+          </Link>
+          <Link href="/">
+            <p className="headerLink">Documentation</p>
+          </Link>
+          <Link href="/">
+            <button className="  headerLink">Sign in</button>
+          </Link>
+          <Link href="/">
+            <button className="headerLink">Sign up</button>
+          </Link>
         </div>
       </div>
     </header>
--- components/Landing.tsx
+++ components/Landing.tsx
@@ -3,16 +3,15 @@ import Image from "next/image";
 
 function Landing() {
   return (
-    <div className=" flex justify-center font-author tracking-wide w-full  h-full">
+    <div className="flex justify-center font-author tracking-wide w-full h-full landingBg">
       <img
-        className="w-full absolute top-0 -z-10 h-screen blur scale-105"
+        className="absolute top-0 -z-10 blur h-screen w-full scale-105"
         src="https://cdn.dribbble.com/users/1770290/screenshots/6158205/bg_75.gif"
         alt=""
       />
-      <div className="absolute">
-        <h1 className="text-white  text-7xl text-center">ICONICNPM</h1>
-        <p className="text-7xl text-center text-white">
-          all the libraries you need,Made simple.
+      <div className="px-20">
+        <p className="text-7xl text-center font-medium heading tracking-wide text-white">
+          All the libraries you need, Made simple.
         </p>
       </div>
     </div>
