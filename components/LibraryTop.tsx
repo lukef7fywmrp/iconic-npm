a8e6206875a47a863ab69b1125e61104304fd66d
lukef7fywmrp <lukef7fywmrp@gmail.com>
2022-09-06 12:26:00 +0400

    added prop types

--- components/LibraryTop.tsx
+++ components/LibraryTop.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Image from "next/image";
 
-function LibraryTop(props) {
+interface Props {
+  libraryLogo: string;
+  libraryName: string;
+  libraryDescription: string;
+}
+
+function LibraryTop(props: Props) {
   return (
     <div className=" text-white px-10 ">
       <div className="flex flex-col justify-center items-center bg-[#1e1e1e] rounded-xl py-5 space-y-2 w-full px-20">
