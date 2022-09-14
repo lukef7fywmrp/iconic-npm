2a2f7e5d058d4154885ac4db91279766b9f45815
lukef7fywmrp <lukef7fywmrp@gmail.com>
2022-09-14 19:22:37 +0400

    added login.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,15 +7,17 @@ function Header() {
       <img className="absolute w-full h-[180vh]" src="/gradientbg.png" alt="" />
       <div className="flex w-full h-full justify-between px-10 py-3 z-0">
         {/* Logo */}
-        <div className="flex items-center space-x-1 transition-transform duration-300 ease-out hover:scale-105 cursor-pointer">
-          <div className="relative w-12 h-12 ">
-            <Image src="/logo6.png" objectFit="contain" layout="fill" />
-          </div>
+        <Link href="/">
+          <div className="flex items-center space-x-1 transition-transform duration-300 ease-out hover:scale-105 cursor-pointer">
+            <div className="relative w-12 h-12 ">
+              <Image src="/logo6.png" objectFit="contain" layout="fill" />
+            </div>
 
-          <p className="text-white text-xl tracking-wider font-author">
-            Iconic
-          </p>
-        </div>
+            <p className="text-white text-xl tracking-wider font-author">
+              Iconic
+            </p>
+          </div>
+        </Link>
         {/* Pricing & Sign in */}
         <div className="flex items-center text-sm space-x-6 text-white/80">
           <Link href="/">
@@ -24,10 +26,10 @@ function Header() {
           <Link href="/">
             <p className="headerLink">Documentation</p>
           </Link>
-          <Link href="/">
+          <Link href="/login">
             <div className="bg-gradient-to-tr from-[#50afeb] to-[#cd0789] p-[1px] rounded-md headerLink">
               <button className="bg-[#1b0026] px-4 py-1 rounded-md">
-                Sign up
+                Log in
               </button>
             </div>
           </Link>
