b5e6d51dd5575dc9c74d1ad880b2780d8327af0a
Brizo699 <97060331+Brizo699@users.noreply.github.com>
2022-09-30 13:43:32 +0800

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="z-10 border-t border-t-white/10">
-      <div className="py-10 ">
+    <div className="border-t border-t-white/10">
+      <div className="py-10">
         <h1 className="text-white/90">Community</h1>
       </div>
     </div>
--- components/LibraryTop.tsx
+++ components/LibraryTop.tsx
@@ -13,8 +13,8 @@ function LibraryTop({
     <Link href={slug.current}>
       <div className="flex w-full flex-col items-center justify-center space-y-3 rounded-2xl bg-white/5 py-5 px-10 transition duration-500 hover:bg-white/10">
         <div className="group flex flex-col items-center space-y-3 px-10 font-author">
-          <div className="relative ">
-            <div className="before:gradient flex h-24 w-24 items-center rounded-full bg-white p-3  before:rounded-full before:border-[2.5px]">
+          <div className="gradientBorder p-[2.5px]">
+            <div className="flex h-24 w-24 items-center rounded-full bg-white p-3  ">
               <Image src={urlFor(logo).url()!} height="100%" width="100%" />
             </div>
           </div>
@@ -42,8 +42,8 @@ function LibraryTop({
           </button>
         </div>
         {/* Button */}
-        <div className="relative">
-          <div className="before:gradient group p-0.5 font-author text-lg before:rounded-full">
+        <div className="gradientBorder p-0.5">
+          <div className="rounded-full bg-[#151515] font-author text-lg">
             <button className="group relative inline-flex items-center justify-start overflow-hidden rounded-full px-5 py-1 font-normal">
               <span className="h-34 absolute left-0 top-0 w-32 translate-x-12 -translate-y-2 rotate-45 bg-gradient-to-tr from-purple-500 to-orange-500 opacity-[0%]"></span>
               <span className="absolute top-0 left-0 -mt-1 h-44 w-48 -translate-x-56 -translate-y-24 rotate-45 bg-gradient-to-tr from-purple-500 to-orange-500 opacity-100 transition-all duration-500 ease-in-out group-hover:-translate-x-8"></span>
